82877c032d0e7e185648cba3106d3472f4ee5a92
KouKeioh <kou2013kou.kou@gmail.com>
2018-01-23 23:38:33 +0900

    オブジェクトのクローン生成テスト

---
+++ test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    public GameObject obj;
+    //public int volume;
+
+	// Use this for initialization
+	void Start () {
+        //volum = 1;
+        for(int n = 0; n < 20; n++)
+        {
+            for (int u = 0; u < 110; u++)
+            {
+                Instantiate(obj, obj.transform.position = new Vector3(-10 + n, u), Quaternion.identity);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
