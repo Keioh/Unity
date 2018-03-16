9d5c786085a5e10c62d92c7bc1d63d8ac0d8474c
KouKeioh <kou2013kou.kou@gmail.com>
2018-03-17 06:01:51 +0900

    動けるけど動かないRPG案(未完)

---
+++ 案/Assets/enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+    public GameObject obj;//複製したいオブジェクト
+    public int volume;//複製する数
+    public float power;//強さ
+   
+    // Use this for initialization
+    void Start () {
+
+        Transform pos = GetComponent<Transform>();
+
+        //volumeがマイナスだったら0で初期化
+        if (volume < 0) volume = 0;
+
+		//オブジェクトを複製
+		for(int n = 0; n < volume; n++)
+        {
+            Instantiate(obj);
+            obj.transform.position = new Vector3(Random.insideUnitSphere.x * power + pos.position.x, Random.insideUnitSphere.y * power + pos.position.y, Random.insideUnitSphere.z * power + pos.position.z);
+        } 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
---
+++ 案/Assets/enemy_anim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_anim : MonoBehaviour {
+
+    Transform trns;
+
+	// Use this for initialization
+	void Start () {
+        trns = GetComponent<Transform>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        trns.position += new Vector3(Random.Range(-0.01f, 0.01f), Random.Range(-0.01f, 0.01f), Random.Range(-0.01f, 0.01f));
+    }
+}
