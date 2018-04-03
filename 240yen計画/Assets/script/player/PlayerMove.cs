10a60666d7a975e8c8e7500f2d3acfb23dea57c3
KouKeioh <kou2013kou.kou@gmail.com>
2018-04-03 16:07:36 +0900

    あぷで

--- 240yen計画/Assets/script/player/PlayerMove.cs
+++ 240yen計画/Assets/script/player/PlayerMove.cs
@@ -6,23 +6,26 @@ public class PlayerMove : MonoBehaviour
 {
 
     //pubilc
-    //public float move_speed;
+    public float move_speed;
 
     //private
     Transform transform;
     Touch touch;
     Vector2 touch_began_position, touch_ended_position, touch_vector;
+    Vector3 began_position, ended_position, position_vector;
+    bool touch_ended_flag;
 
     // Use this for initialization
     void Start()
     {
+        touch_ended_flag = false;
         transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0)//タッチ数が1以上なら
+        if ((Input.touchCount > 0) && (touch_ended_flag == false))//タッチ数が1以上なら(touch_ende_flagは一回のフリックで不確定距離すすまないためのフラグ)
         {
             touch = Input.GetTouch(0);//最初にタッチされたタッチ情報を取得
 
@@ -30,6 +33,7 @@ public class PlayerMove : MonoBehaviour
             {
                 case TouchPhase.Began://タッチししたときの処理
                     touch_began_position = touch.position;//はじめにタッチした座標を保存
+                    began_position = transform.position;//オブジェクトの移動前の位置を保存
                     break;
 
                 case TouchPhase.Stationary://タッチしながら指を移動しているときの処理
@@ -41,30 +45,49 @@ public class PlayerMove : MonoBehaviour
 
                     touch_vector = touch_began_position - touch_ended_position;//座標の差を保存
 
-                    if (Mathf.Abs(touch_vector.x) < Mathf.Abs(touch_vector.y))//Y座標のほうが大きかったら
-                    {
-                        if (touch_vector.y > 100)//下方向
-                        {
-                            transform.Translate(0.0f, -0.5f, 0.0f);
-                        }
-                        if (touch_vector.y < -100)//上方向
-                        {
-                            transform.Translate(0.0f, 0.5f, 0.0f);
-                        }
-                    }
+                    touch_ended_flag = true;
 
-                    if (Mathf.Abs(touch_vector.x) > Mathf.Abs(touch_vector.y))//X座標のほうが大きかったら
-                    {
-                        if (touch_vector.x > 100)//左方向
-                        {
-                            transform.Translate(-0.5f, 0.0f, 0.0f);
-                        }
-                        if (touch_vector.x < -100)//右方向
-                        {
-                            transform.Translate(0.5f, 0.0f, 0.0f);
-                        }
-                    }
                     break;
+            }
+        }
+
+        if (touch_ended_flag == true)
+        {
+            position_vector = began_position - ended_position;//オブジェクトの座標の差を保存
+            if ((Mathf.Abs(position_vector.x) <= 1.0f) || (Mathf.Abs(position_vector.y) <= 1.0f))
+            {
+                //移動処理
+                if (Mathf.Abs(touch_vector.x) < Mathf.Abs(touch_vector.y))//Y座標のほうが大きかったら
+                {
+                    if (touch_vector.y > 100)//下方向
+                    {
+                        transform.Translate(0.0f, -move_speed, 0.0f);
+                    }
+                    if (touch_vector.y < -100)//上方向
+                    {
+                        transform.Translate(0.0f, move_speed, 0.0f);
+                    }
+                }
+
+                if (Mathf.Abs(touch_vector.x) > Mathf.Abs(touch_vector.y))//X座標のほうが大きかったら
+                {
+                    if (touch_vector.x > 100)//左方向
+                    {
+                        transform.Translate(-move_speed, 0.0f, 0.0f);
+                    }
+                    if (touch_vector.x < -100)//右方向
+                    {
+                        transform.Translate(move_speed, 0.0f, 0.0f);
+                    }
+                }
+            }
+
+            ended_position = transform.position;//移動しているオブジェクトの位置を保存
+
+            //移動終了処理
+            if ((Mathf.Abs(position_vector.x) > 1.0f) || (Mathf.Abs(position_vector.y) > 1.0f))
+            {
+                touch_ended_flag = false;
             }
         }
     }
