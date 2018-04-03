299efeaeb85f94b9bbeaa09d09d8049c6521a247
KouKeioh <kou2013kou.kou@gmail.com>
2018-04-04 00:46:56 +0900

    もっと精密に移動できるように処理を書き直さないといけない

--- 240yen計画/Assets/script/player/PlayerMove.cs
+++ 240yen計画/Assets/script/player/PlayerMove.cs
@@ -9,11 +9,11 @@ public class PlayerMove : MonoBehaviour
     public float move_speed;
 
     //private
-    Transform transform;
-    Touch touch;
-    Vector2 touch_began_position, touch_ended_position, touch_vector;
-    Vector3 began_position, ended_position, position_vector;
-    bool touch_ended_flag;
+    Transform transform;//オブジェクトのtransform
+    Touch touch;//タッチ情報
+    Vector2 touch_began_position, touch_ended_position, touch_vector;//タッチの座標保存
+    Vector3 began_position, ended_position, position_vector;//オブジェクトの座標保存
+    bool touch_ended_flag;//タッチがおわったときのフラグ
 
     // Use this for initialization
     void Start()
@@ -36,8 +36,7 @@ public class PlayerMove : MonoBehaviour
                     began_position = transform.position;//オブジェクトの移動前の位置を保存
                     break;
 
-                case TouchPhase.Stationary://タッチしながら指を移動しているときの処理
-
+                case TouchPhase.Stationary://タッチしているが動かさないときの処理
                     break;
 
                 case TouchPhase.Ended://タッチ終わり
@@ -54,18 +53,19 @@ public class PlayerMove : MonoBehaviour
         if (touch_ended_flag == true)
         {
             position_vector = began_position - ended_position;//オブジェクトの座標の差を保存
-            if ((Mathf.Abs(position_vector.x) <= 1.0f) || (Mathf.Abs(position_vector.y) <= 1.0f))
+
+            if ((Mathf.Abs(position_vector.x) <= 1.0f) || (Mathf.Abs(position_vector.z) <= 1.0f))
             {
                 //移動処理
                 if (Mathf.Abs(touch_vector.x) < Mathf.Abs(touch_vector.y))//Y座標のほうが大きかったら
                 {
                     if (touch_vector.y > 100)//下方向
-                    {
-                        transform.Translate(0.0f, -move_speed, 0.0f);
+                    {                        
+                        transform.Translate(0.0f, 0.0f, -move_speed);
                     }
                     if (touch_vector.y < -100)//上方向
                     {
-                        transform.Translate(0.0f, move_speed, 0.0f);
+                        transform.Translate(0.0f, 0.0f, move_speed);
                     }
                 }
 
@@ -85,7 +85,7 @@ public class PlayerMove : MonoBehaviour
             ended_position = transform.position;//移動しているオブジェクトの位置を保存
 
             //移動終了処理
-            if ((Mathf.Abs(position_vector.x) > 1.0f) || (Mathf.Abs(position_vector.y) > 1.0f))
+            if ((Mathf.Abs(position_vector.x) > 1.0f) || (Mathf.Abs(position_vector.z) > 1.0f))
             {
                 touch_ended_flag = false;
             }
