a12ac529c18d93c9aace392713be97247c291401
KouKeioh <kou2013kou.kou@gmail.com>
2021-10-21 11:12:31 +0900

    AssetBundleの勉強
    AssetBundleLoaderでAssetBundleをListに読み込んで他のスクリプトから使用する。
    Unityのvoid Start()の仕様に沼ることないように。
    test.csはAssetBundleLoaderからどうやって読み込むのかのサンプル。

---
+++ Script/AssetBundle/AssetBundleLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetBundleLoader : MonoBehaviour
+{
+    [SerializeField, HeaderAttribute("AssetBundleパス"), Tooltip("AssetBundleへのパスを追加します。")]
+    private List<string> assetBundlePath;//アセットバンドルのパス(実際のオブジェクト)
+
+    private AssetBundle assetBundle;//asset_bundleの一時保存用。
+
+    private bool loadFinishFlag = false;//読み込みが終ったかのフラグ
+
+    [HeaderAttribute("読み込み済みAssetBundle"), Tooltip("読み込まれたAssetBundleの一覧です。")]//インスペクター上で見えるprivate群
+    public List<AssetBundle> assetBundleList;//アセットバンドルのリスト
+
+
+
+    //読み込みが完了したらtrueを返す関数。
+    public bool LoadFinish()
+    {
+        return loadFinishFlag;//読み込み終了フラグを返す。
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {         
+        loadFinishFlag = false;//読み込みフラグをfalseにする。
+
+        for (int count = 0; count != assetBundlePath.Count; ++count)//asset_bundle_pathの数だけ繰り返す。
+        {
+
+            assetBundle = AssetBundle.LoadFromFile(assetBundlePath[count]);//アセットバンドルを読み込む
+
+            assetBundleList.Add(assetBundle);//読み込んだアセットバンドルをasset_bundle_listに追加。
+        }
+
+        loadFinishFlag = true;//読み込みフラグをtrueにする。
+    }
+}
---
+++ Script/AssetBundle/test.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [SerializeField, HeaderAttribute("AssetBundle一覧"), Tooltip("読み込まれたAssetBundleの一覧です。")]
+    private List<AssetBundle> assetBundles;//アセットバンドル一覧表示用
+    private AssetBundleLoader assetBundleLoaderScript;//アセットバンドルローダースクリプト参照用。
+
+    [SerializeField, HeaderAttribute("生成済みObject"), Tooltip("生成したObjectです。")]
+    private GameObject gameObject;//生成するオブジェクト用。
+
+    private bool objectInstansFlag = false;//オブジェクト生成フラグ(生成したらtrue)
+    private bool objectDestoryFlag = false;//オブジェクト生成フラグ(生成したらtrue)
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        assetBundleLoaderScript = GameObject.Find("AssetBundleLoader").GetComponent<AssetBundleLoader>();//AssetBundleLoaderオブジェクトからデータを取得
+        assetBundles = assetBundleLoaderScript.assetBundleList;//読み込んだアセットバンドルの一覧を取得
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((objectInstansFlag == false) && (assetBundleLoaderScript.LoadFinish() == true))//読み込みが終っていてかつオブジェクト生成していないのであれば
+        {
+            gameObject = assetBundleLoaderScript.assetBundleList[0].LoadAsset<GameObject>("town");//オブジェクトを読み込む
+
+            Instantiate(gameObject);//オブジェクト生成
+
+            objectInstansFlag = true;//オブジェクト生成フラグを立てる(trueにする)
+        }
+
+        //objectDestoryFlagがtrueなら
+        if (objectDestoryFlag == true)
+        {
+            Destroy(gameObject);//オブジェクトを破棄
+
+            objectInstansFlag = false;//オブジェクト生成フラグを伏せる(falseにする)
+        }
+    }
+
+    //オブジェクトを破棄する。
+    public void ObjectDestory()
+    {
+        if (gameObject != null)
+        {
+            objectDestoryFlag = true;
+        }
+        else
+        {
+            objectDestoryFlag = false;
+        }
+    }
+}
