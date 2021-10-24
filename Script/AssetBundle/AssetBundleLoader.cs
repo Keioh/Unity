248d0c4c0d007888990fdfe4983f6f23bf5618bd
KouKeioh <kou2013kou.kou@gmail.com>
2021-10-24 14:13:41 +0900

    更新

--- Script/AssetBundle/AssetBundleLoader.cs
+++ Script/AssetBundle/AssetBundleLoader.cs
@@ -5,10 +5,13 @@ using System.IO;
 
 public class AssetBundleLoader : MonoBehaviour
 {
+    [SerializeField, HeaderAttribute("非同期読み込み設定"), Tooltip("チェックするとAssetBundleの読み込みを非同期で行います。")]
+    private bool async = true;//trueで非同期読み込み
+
     [SerializeField, HeaderAttribute("AssetBundleパス"), Tooltip("AssetBundleへのパスを追加します。")]
     private List<string> assetBundlePath;//アセットバンドルのパス(実際のオブジェクト)
 
-    private AssetBundleCreateRequest assetBundleCreateRequest;
+    private AssetBundleCreateRequest assetBundleCreateRequest;//AssetBundleの非同期読み込み用
     private AssetBundle assetBundle;//asset_bundleの一時保存用。
 
     private bool loadFinishFlag = false;//読み込みが終ったかのフラグ
@@ -24,6 +27,15 @@ public class AssetBundleLoader : MonoBehaviour
         return loadFinishFlag;//読み込み終了フラグを返す。
     }
 
+    //読み込み設定を変更する関数。(trueで非同期読み込み)
+    public bool SetAsync(bool newFlag)
+    {
+        async = newFlag;
+
+        return async;//新しいフラグを返す
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {         
@@ -31,9 +43,21 @@ public class AssetBundleLoader : MonoBehaviour
 
         for (int count = 0; count != assetBundlePath.Count; ++count)//asset_bundle_pathの数だけ繰り返す。
         {
-            assetBundleCreateRequest = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(assetBundlePath[count]));//非同期読み込み処理(同期読み込みの場合はこの行を削除し、下の行のコメント部分をassetBundleCreateRequest部分と入れ替える。)
+            //AssetBundleの非同期読み込み設定がtrueなら非同期読み込みをする。
+            if (async == true)
+            {
+                Debug.Log("AssetBundleLoader:非同期読み込み [" + (count + 1) + "個目(Element" + count + ")]");
 
-            assetBundle = assetBundleCreateRequest.assetBundle;//AssetBundle.LoadFromFile(assetBundlePath[count]);//アセットバンドルを読み込む
+                assetBundleCreateRequest = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(assetBundlePath[count]));//非同期読み込み処理(同期読み込みの場合はこの行を削除し、下の行のコメント部分をassetBundleCreateRequest部分と入れ替える。)
+
+                assetBundle = assetBundleCreateRequest.assetBundle;//AssetBundle.LoadFromFile(assetBundlePath[count]);//アセットバンドルを読み込む
+            }
+            else
+            {
+                Debug.Log("AssetBundleLoader:同期読み込み [" + (count + 1) + "個目(Element" + count + ")]");
+
+                assetBundle = AssetBundle.LoadFromFile(assetBundlePath[count]);//アセットバンドルを読み込む
+            }
 
             assetBundleList.Add(assetBundle);//読み込んだアセットバンドルをasset_bundle_listに追加。
         }
--- Script/AssetBundle/test.cs
+++ Script/AssetBundle/test.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class test : MonoBehaviour
-{
-    [SerializeField, HeaderAttribute("AssetBundle一覧"), Tooltip("読み込まれたAssetBundleの一覧です。")]
-    private List<AssetBundle> assetBundles;//アセットバンドル一覧表示用
-    private AssetBundleLoader assetBundleLoaderScript;//アセットバンドルローダースクリプト参照用。
-
+{   
     [SerializeField, HeaderAttribute("生成済みObject"), Tooltip("生成したObjectです。")]
     private GameObject gameObject;//生成するオブジェクト用。
+
+    [SerializeField, HeaderAttribute("AssetBundle一覧"), Tooltip("読み込まれたAssetBundleの一覧です。")]
+    private List<AssetBundle> assetBundles;//アセットバンドル一覧表示用
+    private AssetBundleLoader assetBundleLoader;//アセットバンドルローダースクリプト参照用。
 
     private bool objectInstansFlag = false;//オブジェクト生成フラグ(生成したらtrue)
     private bool objectDestoryFlag = false;//オブジェクト生成フラグ(生成したらtrue)
@@ -18,16 +18,16 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        assetBundleLoaderScript = GameObject.Find("AssetBundleLoader").GetComponent<AssetBundleLoader>();//AssetBundleLoaderオブジェクトからデータを取得
-        assetBundles = assetBundleLoaderScript.assetBundleList;//読み込んだアセットバンドルの一覧を取得
+        assetBundleLoader = GameObject.Find("AssetBundleLoader").GetComponent<AssetBundleLoader>();//AssetBundleLoaderオブジェクトからデータを取得
+        assetBundles = assetBundleLoader.assetBundleList;//読み込んだアセットバンドルの一覧を取得
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((objectInstansFlag == false) && (assetBundleLoaderScript.LoadFinish() == true))//読み込みが終っていてかつオブジェクト生成していないのであれば
+        if ((objectInstansFlag == false) && (assetBundleLoader.LoadFinish() == true))//読み込みが終っていてかつオブジェクト生成していないのであれば
         {
-            gameObject = assetBundleLoaderScript.assetBundleList[0].LoadAsset<GameObject>("town");//オブジェクトを読み込む
+            gameObject = assetBundleLoader.assetBundleList[0].LoadAsset<GameObject>("town");//オブジェクトを読み込む
 
             Instantiate(gameObject);//オブジェクト生成
 
